590328040d957196eb364e295938b681b5ffad74
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-06-18 11:44:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ API_HASH = environ.get("API_HASH", "")  # api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "")  # bot token
 
 # REDIS
-REDIS_HOST = environ.get("REDIS_HOST", "localhost")  # redis host uri
-REDIS_PORT = environ.get("REDIS_PORT", 12345)  # redis port
+REDIS_HOST = environ.get("REDIS_HOST", "redis-15392.c304.europe-west1-2.gce.cloud.redislabs.com")  # redis host uri
+REDIS_PORT = environ.get("REDIS_PORT", 15392)  # redis port
 REDIS_PASSWORD = environ.get(
-    "REDIS_PASSWORD", "q8vcRgGfoZgwKT6irvJ6AixPs1lFZdW8"
+    "REDIS_PASSWORD", "cbw7xHuW3uJmddWJRRqxiSWmRvKi81GM"
 )  # redis password
 
 
-ADMINS = [6791744215]
-OWNER_ID = 6791744215  # Replace with your Telegram user ID
+ADMINS = [5821871362]
+OWNER_ID = 5821871362  # Replace with your Telegram user ID
 PRIVATE_CHAT_ID = -100  # CHAT WHERE YOU WANT TO STORE VIDEOS
 USER_CHANNEL = -100
 DUMP_CHANNEL = -100
